0700f94b1fb20e303d2efef71aea5bbd253f451a
pierremahot <pierre.mahot@orange.fr>
2019-08-24 18:47:19 +0200

    adeline line

--- screens/FavoritesScreen.js
+++ screens/FavoritesScreen.js
@@ -26,6 +26,12 @@ export default function FavoritesScreen({ navigation }) {
       <Hours refresh={refreshState} bus="C2" stop="République" destination="Saint-Grégoire" />
       <Hours refresh={refreshState} bus="C2" stop="Champ Daguet" destination="Haut Sancé" />
       <Hours refresh={refreshState} bus="C6" stop="République" destination="Aéroport" />
+      <Hours refresh={refreshState} bus="C3" stop="République" destination="Saint-Laurent" />
+      <Hours refresh={refreshState} bus="C3" stop="Le Blizz" destination="Henri Fréville" />
+      <Hours refresh={refreshState} bus="C5" stop="République" destination="Lycée Bréquigny" />
+      <Hours refresh={refreshState} bus="14" stop="La Plesse" destination="Beaulieu" />
+      <Hours refresh={refreshState} bus="78" stop="Edonia" destination="Rennes Villejean-Université" />
+      <Hours refresh={refreshState} bus="57" stop="République" destination="Bruz" />
     </RefreshControlScrollView>
   );
 }
